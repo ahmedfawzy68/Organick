18ee1a4893da35343674b73915674abab4c2afed
ahmedfawzy68 <ahmedfawzy6820@gmail.com>
2023-10-29 13:25:03 +0200

    V1.5

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,16 +1,10 @@
-import { Component, AfterViewInit } from '@angular/core';
-declare var $: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css'],
+  styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements AfterViewInit {
-  ngAfterViewInit() {
-    // Initialize the carousel
-    $(document).ready(function () {
-      $('#carouselExampleIndicators').carousel();
-    });
-  }
+export class HomepageComponent {
+
 }
